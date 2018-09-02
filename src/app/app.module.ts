2b93d7d4e1fba6e1357fe27a776184d87f738e04
svetlana <zelenvasveta94@gmail.com>
2018-09-02 21:35:36 +0300

    add packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { AngularFontAwesomeModule } from "angular-font-awesome";
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFontAwesomeModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
