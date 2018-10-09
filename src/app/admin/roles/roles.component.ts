6e53672f0bc22f6496bf4d16b57f54cf5474dbf2
svetlana <zelenvasveta94@gmail.com>
2018-10-09 14:10:19 +0300

    roles component and service with ability to add/update/delete roles

--- src/app/admin/roles/roles.component.ts
+++ src/app/admin/roles/roles.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Role} from "../common.interfaces";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {RolesService} from "./roles.service";
+import {ConfirmationService, MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-roles',
@@ -7,9 +11,100 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RolesComponent implements OnInit {
 
-  constructor() { }
+  roles: Role[];
+  roleForm: FormGroup;
+  isRefreshing: boolean = false;
+  submitted: boolean = false;
+  displayRoleDialog: boolean = false;
+
+  constructor(private rolesService: RolesService,
+              private formBuilder: FormBuilder,
+              private messageService: MessageService,
+              private confirmationService: ConfirmationService) { }
 
   ngOnInit() {
-  }
+    this.loadRoles(false);
 
+    this.roleForm = this.formBuilder.group({
+      id: [0, Validators.required],
+      name: ['', Validators.required]
+    })
+  }
+  loadRoles(showMessage: boolean = true) {
+    this.isRefreshing = true;
+    this.rolesService.getAllRoles()
+      .subscribe(data => {
+        this.roles = data.roles;
+        this.isRefreshing = false;
+        if (!showMessage) {
+          return;
+        }
+        this.messageService.add({
+          severity: 'info',
+          summary: 'Roles table refreshed'
+        })
+      })
+  }
+  displayRoleForm(role? : Role) {
+    this.submitted = false;
+    const roleForm = role ? role : {id: 0, name: ''};
+    this.roleForm.reset(roleForm)
+    this.displayRoleDialog = true;
+  }
+  hideRoleForm() {
+    this.displayRoleDialog = false;
+  }
+  submitRole() {
+    this.submitted = true;
+    const role = this.roleForm.value as Role;
+    if (!this.roleForm.valid) {
+      return;
+    }
+    if (role.id === 0) {
+      this.createRole(role);
+    } else {
+      this.updateRole(role);
+    }
+  }
+  createRole(role: Role) {
+    this.rolesService.createRole(role)
+      .subscribe(role => {
+        this.loadRoles(false);
+        this.hideRoleForm();
+        this.messageService.add({
+          severity: 'info',
+          summary: `Role '${role.name}' successfully created`
+        })
+      },
+        error => console.log(error))
+  }
+  updateRole(role: Role) {
+    this.rolesService.updateRole(role)
+      .subscribe(role => {
+        this.loadRoles(false);
+        this.hideRoleForm();
+        this.messageService.add({
+          severity: 'info',
+          summary: `Role '${role.name}' successfully updated`
+        })
+      },
+        error => console.log(error))
+  }
+  deleteRole(id: number) {
+    const role = this.roles.find(r => r.id === id)
+    this.confirmationService.confirm({
+      message: `Are you shure want to delete role '${role.name}'`,
+      accept:() => {
+        this.rolesService.deleteRole(id)
+          .subscribe(() => {
+              this.loadRoles(false);
+              this.messageService.add({
+                severity: 'info',
+                summary: `Role '${role.name}' successfully removed`
+              })
+            },
+            error => console.log(error))
+      }
+    });
+  }
 }
--- src/app/admin/roles/roles.service.ts
+++ src/app/admin/roles/roles.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ApiResponse, Role} from "../common.interfaces";
+import {catchError, map} from "rxjs/operators";
+import {HttpErrorHandler, HandleError} from "../../core/http-error-handler.service";
+import {Observable} from "rxjs/index";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RolesService {
 
-  constructor() { }
+  private readonly handleError: HandleError;
+  private readonly rolePath: string;
+  constructor(
+    private http: HttpClient,
+    httpErrorHandler: HttpErrorHandler) {
+    this.handleError = httpErrorHandler.createHandleError('RolesService');
+    this.rolePath = environment.API_BASE_PATH + '/roles/';
+  }
+  getAllRoles(): Observable<{roles: Role[]}> {
+    return this.http.get<ApiResponse<Role[]>>(this.rolePath)
+      .pipe(
+        map((data) => {
+          const roles = data.payload as Role[];
+          return {roles: roles}
+        })
+      )
+  }
+  createRole(role: Role): Observable<Role> {
+    return this.http.post<ApiResponse<Role>>(this.rolePath, role)
+      .pipe(
+        map( data => ({
+          id: data.payload.id,
+          name: data.payload.name})),
+        catchError(this.handleError<Role>('createRole', role))
+      )
+  }
+  updateRole(role: Role): Observable<Role> {
+    return this.http.put<ApiResponse<Role>>(this.rolePath + role.id, role)
+      .pipe(
+        map(data => ({
+          id: data.payload.id,
+          name: data.payload.name})),
+        catchError(this.handleError<Role>('updateRole', role))
+      );
+  }
+  deleteRole(id: number): Observable<number> {
+    return this.http.delete<ApiResponse<any>>(this.rolePath + id)
+      .pipe(
+        map(data => data.service.successful ? id : 0),
+        catchError(this.handleError<number>('deleteDoor', id))
+      );
+  }
 }
